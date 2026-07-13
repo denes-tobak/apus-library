3d8fae82a8c8bb7c8e433b7f0ee346e744489051
Denes Tobak <tobakdenes@gmail.com>
2026-07-13 22:57:01 +0200

    DB Seed initial 5 record

---
+++ docs/database/seed/001_seed_books.sql
@@ -0,0 +1,43 @@
+insert into public.books (
+    title,
+    author,
+    published_year,
+    category,
+    series_number
+)
+values
+(
+    'The Hobbit',
+    'J.R.R. Tolkien',
+    1937,
+    'Fantasy',
+    1
+),
+(
+    'Dune',
+    'Frank Herbert',
+    1965,
+    'Science Fiction',
+    1
+),
+(
+    '1984',
+    'George Orwell',
+    1949,
+    'Dystopian',
+    0
+),
+(
+    'The Name of the Wind',
+    'Patrick Rothfuss',
+    2007,
+    'Fantasy',
+    1
+),
+(
+    'The Way of Kings',
+    'Brandon Sanderson',
+    2010,
+    'Fantasy',
+    1
+);
